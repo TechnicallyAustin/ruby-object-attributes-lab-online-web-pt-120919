b01d4e289439cdb43cd15f0fae4c79b9d1c8d434
Austin Johnson <technicallyaustin@gmail.com>
2020-01-03 01:59:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+  def name= (person_name)
+    @this_person = person_name
+  end
+  
+  def name 
+    @this_person
+  end
+  
+  def job= (person_job)
+    @this_job = person_job
+  end
+  
+  def job 
+    @this_job
+  end
   
 end
