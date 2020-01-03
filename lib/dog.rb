4c648de860a35fdfa02017c9af8e406a4d26dd19
Austin Johnson <technicallyaustin@gmail.com>
2020-01-03 01:58:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,17 @@ class Dog
     @this_dog
   end
   
-  def name(new
-    @this_dog = 
+  def name= (new_name)
+    @this_dog = new_name
+  end
+  
+  def breed= (dog_breed)
+    @this_breed = dog_breed
+  end
+  
+  def breed
+    @this_breed
+  end
+    
   
 end
