be8afee1ab064178521bedc7d8cd1f9bd137d93b
Austin Johnson <technicallyaustin@gmail.com>
2020-01-03 02:03:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog 
   def name= (dogs_name)
-    @this_dog = dogs_name.capitalize
+    @name = dogs_name.capitalize
   end
   
   def name 
-    @this_dog
+    @name
   end
   
   def name= (new_name)
-    @this_dog = new_name
+    @name = new_name
   end
   
   def breed= (dog_breed)
-    @this_breed = dog_breed
+    @breed = dog_breed
   end
   
   def breed
-    @this_breed
+    @breed
   end
     fido = Dog.new 
     fido.name
